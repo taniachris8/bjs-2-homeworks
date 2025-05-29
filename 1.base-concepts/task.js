c5a901b33b158fd6ec3a90dbe6503e1c9146ef6b
taniachris8 <dove6464dove@gmail.com>
2025-05-29 11:46:32 +0300

    first task solved

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  const discriminant = b ** 2 - 4 * a * c;
+
+  if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  } else if (discriminant > 0) {
+    arr.push(
+      (-b + Math.sqrt(discriminant)) / (2 * a),
+      (-b - Math.sqrt(discriminant)) / (2 * a)
+    );
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  const monthlyRate = percent / 100 / 12;
+  const loanPrincipal = amount - contribution;
+  const monthlyPayment =
+    loanPrincipal *
+    (monthlyRate + monthlyRate / ((1 + monthlyRate) ** countMonths - 1));
+  const totalAmountToPay = monthlyPayment * countMonths;
+
+  return Number(totalAmountToPay.toFixed(2));
 }
